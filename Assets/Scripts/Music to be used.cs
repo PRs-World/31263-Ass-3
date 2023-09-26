02af4b4232233258e7ea8ae70fceda1dab59b7d4
Pat Ramses Rulete Labirua <patramseslab@yahoo.com>
2023-09-26 16:25:11 +1000

    Final commit test 09/26/23
    Final commit test with another C# script for music choices so far, 09/26/23 16:25.

---
+++ Assets/Scripts/Music to be used.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Musictobeused : MonoBehaviour
+{
+    // Start is called before the first frame update
+    /*void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }*/
+    //Just another script to finalize commit function before leaving UTS 09/26/23.
+    //Anyway, here's the music I thought I'll be using so far for the assignment.
+    //Background music for the game intro (when the level first starts) - Three 3 by Far Orange from https://www.epidemicsound.com/music/genres/electronica-dance/
+    //Sound effect for when a pellet is eaten - Smartphone UI - Text Message, Notification, High Pitch from https://artlist.io/sfx?utm_source=google&utm_medium=cpc&utm_campaign=13681196598&utm_content=123416484181&utm_term=sound%20effects&keyword=sound%20effects&ad=542275939584&matchtype=p&device=c&gclid=CjwKCAjw38SoBhB6EiwA8EQVLrnnFQThlovYEmqNalanSHnjaBZMgL4OKFUsSD-O1SYWsBc2_99yZhoCf6wQAvD_BwE
+
+}
